aa37b0f024a2c96997777b77d9c186e88dc1e885
César Fontalvo <cafc2030@gmail.com>
2024-03-15 02:49:02 -0500

    add:: creates component to delete from cart

---
+++ src/components/DeleteCart.tsx
@@ -0,0 +1,16 @@
+import { MdDeleteForever } from "react-icons/md";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+function DeleteCart({ ...props }: Props) {
+  return (
+    <button
+      {...props}
+      className="self-end flex justify-center items-center gap-2 text-neutral-400 hover:bg-neutral-200 transition ease-in-out px-3 py-2 rounded"
+    >
+      <MdDeleteForever />
+    </button>
+  );
+}
+
+export default DeleteCart;
