9cb23f8bc70cb6a0ef1f05d003dd58c6801446fc
César Fontalvo <cafc2030@gmail.com>
2024-03-15 02:49:20 -0500

    change:: implements delete

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,8 @@
 import { useDispatch } from "react-redux";
-import { addToCart } from "../actions";
+import { addToCart, deleteFromCart } from "../actions";
 import { formatCurrency } from "../utils/utils";
 import AddToCartButton from "./AddToCartButton";
+import DeleteCart from "./DeleteCart";
 
 type ProductCardProps = {
   id: string;
@@ -9,7 +10,7 @@ type ProductCardProps = {
   price: number;
   description: string;
   qty?: number;
-  isCart?: boolean;
+  isInCart?: boolean;
 };
 
 function ProductCard({
@@ -18,21 +19,24 @@ function ProductCard({
   price,
   description,
   qty,
-  isCart = true,
+  isInCart = false,
 }: ProductCardProps) {
   const dispatch = useDispatch();
   const handleCart = () => {
     dispatch(addToCart(id));
   };
+  const handleDelete = () => {
+    dispatch(deleteFromCart(id))
+  }
   return (
-    <div className="flex flex-col gap-2 bg-white shadow-md p-4 rounded-lg">
+    <div className="min-w-36 flex flex-col gap-2 bg-white shadow-md p-4 rounded-lg">
       <div className="flex justify-between gap-2 font-bold">
         <h3>{name}</h3>
         <p>{formatCurrency(price)}</p>
       </div>
       <p className="text-sm text-neutral-400">{description}</p>
-      {qty ? <p className="self-start rounded bg-neutral-400 text-center px-3 py-1">{qty}</p> : null}
-      {isCart && <AddToCartButton onClick={handleCart} />}
+      {qty ? <p className="self-start rounded bg-neutral-200 text-center px-3 py-1">{qty}</p> : null}
+      {isInCart ? <DeleteCart onClick={handleDelete} /> : <AddToCartButton onClick={handleCart} />}
     </div>
   );
 }
