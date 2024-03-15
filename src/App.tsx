751998f47261a0fe86478c5f75915b896233ad6b
César Fontalvo <cafc2030@gmail.com>
2024-03-15 02:50:09 -0500

    change:: updates components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import {Provider} from 'react-redux'
 import store from './store'
+import Products from './components/Products'
+import CreditCardInfo from './components/CreditCardInfo'
 
 function App() {
 
   return (
     <Provider store={store}>
      <h1>Hello World!</h1>
+     <Products />
+     <CreditCardInfo />
     </Provider>
   )
 }
