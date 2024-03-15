33bb7cb5a921e60815971132e31f7b75881d7a53
César Fontalvo <cafc2030@gmail.com>
2024-03-15 02:49:54 -0500

    change:: updates action types and functions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,14 @@
-import { ADD_TO_CART } from "../types";
+import { ADD_TO_CART, REMOVE_ONE_FROM_CART } from "../types";
 
-export type addToCartAction = {
+export type TaddToCartAction = {
     type: typeof ADD_TO_CART
     payload: string
 }
 
-export const addToCart = (id: string): addToCartAction => ({type: ADD_TO_CART, payload: id});
+export const addToCart = (id: string): TaddToCartAction => ({type: ADD_TO_CART, payload: id});
+
+export type TdeleteFromCart = {
+    type: typeof REMOVE_ONE_FROM_CART
+    payload: string
+}
+export const deleteFromCart = (id: string): TdeleteFromCart => ({ type: REMOVE_ONE_FROM_CART, payload: id })
