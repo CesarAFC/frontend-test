d757a5ef1a4565fec9d6d5e277a9b7b4982e8d65
César Fontalvo <cafc2030@gmail.com>
2024-03-16 17:32:25 -0500

    add:: creates reusable empty state

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,18 @@
+import { HiMiniArchiveBoxXMark } from "react-icons/hi2";
+import { IconContext } from "react-icons";
+type EmptyStateProps = {
+  label?: string;
+};
+
+function EmptyState({ label = "No data" }: EmptyStateProps) {
+  return (
+    <section className="h-full flex flex-col justify-center items-center gap-0 text-neutral-300 text-lg font-bold">
+      <IconContext.Provider value={{ size: "2em" }}>
+        <HiMiniArchiveBoxXMark />
+      </IconContext.Provider>
+      <p>{label}</p>
+    </section>
+  );
+}
+
+export default EmptyState;
