970c61bdfb4f778d48f27055a82556953e586b72
César Fontalvo <cafc2030@gmail.com>
2024-03-16 01:15:37 -0500

    change:: updates home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import './App.css'
-import {Provider} from 'react-redux'
-import store from './store'
-import Products from './components/Products'
-import CreditCardInfo from './components/CreditCardInfo'
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import Products from "./UI/Products";
 
 function App() {
 
   return (
     <Provider store={store}>
-     <h1>Hello World!</h1>
-     <Products />
-     <CreditCardInfo />
+      <Products />
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
