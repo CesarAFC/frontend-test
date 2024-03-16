7b2f8676eb0fb055f2a5a55c6d93eef42bc6a28f
César Fontalvo <cafc2030@gmail.com>
2024-03-16 17:33:23 -0500

    change:: updates state for modal and drawer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_ONE_FROM_CART, NEW_CARD_INFO } from "../types";
+import { ADD_TO_CART, REMOVE_ONE_FROM_CART, NEW_CARD_INFO, TOGGLE_CART_DRAWER, TOGGLE_PAYMENT_MODAL } from "../types";
 import { CardInformation } from "../types/store.types";
 
 export type TaddToCartAction = {
@@ -19,3 +19,15 @@ export type TnewCardInfo = {
     payload: CardInformation,
 }
 export const newCardInfo = (card: CardInformation): TnewCardInfo => ({type: NEW_CARD_INFO, payload: card})
+
+export type ToggleCartDrawer = {
+    type: typeof TOGGLE_CART_DRAWER,
+    payload: boolean
+}
+export const toggleCartDrawer = (status: boolean): ToggleCartDrawer => ({type: TOGGLE_CART_DRAWER, payload: status});
+
+export type TogglePaymentModal = {
+    type: typeof TOGGLE_PAYMENT_MODAL,
+    payload: boolean
+}
+export const togglePaymentModal = (status: boolean): TogglePaymentModal => ({type: TOGGLE_PAYMENT_MODAL, payload: status});
