97a399634215984e95dc8349f60ed347b7f1fc2e
César Fontalvo <cafc2030@gmail.com>
2024-03-16 01:36:12 -0500

    change:: updates provider config for persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import { Provider } from "react-redux";
-import store from "./store";
+// import store from "./store";
 import Products from "./UI/Products";
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './store';
+
+const {store, persistor} = configureStore()
 
 function App() {
-
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <Products />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,21 @@
 import {createStore} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
 import reducer from '../reducers';
+import storage from 'redux-persist/lib/storage'
 
-const store = createStore(reducer);
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['shopping'],
+  }
 
-export default store;
+  const persistedReducer = persistReducer(persistConfig, reducer)
+
+// const store = createStore(reducer);
+
+export default () => {
+    let store = createStore(persistedReducer)
+    let persistor = persistStore(store)
+    return { store, persistor }
+  }
+// export default store;
