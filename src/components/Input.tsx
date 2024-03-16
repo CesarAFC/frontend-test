47c8935744e96e0589ac2ece251b38ba5235647c
César Fontalvo <cafc2030@gmail.com>
2024-03-16 01:10:54 -0500

    change:: updates styles for input and label

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,7 @@
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 function Input(props: Props) {
-  return <input {...props} className="p-2 rounded" />;
+  return <input {...props} className="p-2 rounded border border-slate-300 outline-slate-500" />;
 };
 
 export default Input;
--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,6 +1,6 @@
 interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {}
 function Label( {children, ...props}: Props ) {
- return <label {...props}>{children}</label>;
+ return <label className="text-[#737B94] text-base" {...props}>{children}</label>;
 };
 
 export default Label;
