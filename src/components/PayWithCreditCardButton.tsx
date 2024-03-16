1ef2f528c242f616b61dbdc888dc1be1836b7dd4
César Fontalvo <cafc2030@gmail.com>
2024-03-16 01:10:34 -0500

    change:: renames component

--- src/components/PayWithCreditCardButton.tsx
+++ src/components/PayWithCreditCardButton.tsx
@@ -1,17 +1,17 @@
-import { MdAddShoppingCart } from "react-icons/md";
+import { CiCreditCard1 } from "react-icons/ci";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-function AddToCartButton({ ...props }: Props) {
+function PayWithCreditCardButton({ ...props }: Props) {
   return (
     <button
       {...props}
       className="flex justify-center items-center gap-2 bg-indigo-700 text-white hover:bg-indigo-500 transition ease-in-out px-3 py-2 rounded"
     >
-      <MdAddShoppingCart />
-      Add to cart
+      <CiCreditCard1 />
+      <span>Pay with credit card</span>
     </button>
   );
 }
 
-export default AddToCartButton;
+export default PayWithCreditCardButton;
