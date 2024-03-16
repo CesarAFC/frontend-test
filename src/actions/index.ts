3fe724d0a88a5b33e0ecf8ec0368181792038528
César Fontalvo <cafc2030@gmail.com>
2024-03-16 01:16:06 -0500

    change:: updates new card action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
-import { ADD_TO_CART, REMOVE_ONE_FROM_CART } from "../types";
+import { ADD_TO_CART, REMOVE_ONE_FROM_CART, NEW_CARD_INFO } from "../types";
+import { CardInformation } from "../types/store.types";
 
 export type TaddToCartAction = {
     type: typeof ADD_TO_CART
@@ -12,3 +13,9 @@ export type TdeleteFromCart = {
     payload: string
 }
 export const deleteFromCart = (id: string): TdeleteFromCart => ({ type: REMOVE_ONE_FROM_CART, payload: id })
+
+export type TnewCardInfo = {
+    type: typeof NEW_CARD_INFO,
+    payload: CardInformation,
+}
+export const newCardInfo = (card: CardInformation): TnewCardInfo => ({type: NEW_CARD_INFO, payload: card})
