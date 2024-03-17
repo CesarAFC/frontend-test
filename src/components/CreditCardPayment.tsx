437bae9243a227f185c58373b457d3dbbdb975dd
César Fontalvo <cafc2030@gmail.com>
2024-03-17 16:07:50 -0500

    change:: updates form submit trigger

--- src/components/CreditCardPayment.tsx
+++ src/components/CreditCardPayment.tsx
@@ -19,7 +19,6 @@ import { luhnValidation } from "../utils/utils";
 
 export default function CreditCardPayment({ id }: { id: string }) {
   const { isModalPaymentOpen, cardInformation } = useTypedShoppingSelector();
-
   const dispatch = useDispatch();
 
   const handleClickOpen = () => {
@@ -30,8 +29,7 @@ export default function CreditCardPayment({ id }: { id: string }) {
     dispatch(togglePaymentModal(false));
   };
 
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleFormSubmit = () => {
     const isValidCard = luhnValidation(cardInformation.card.trim())
     if (isValidCard) {
       dispatch(togglePaymentCompleted(false));
@@ -55,10 +53,6 @@ export default function CreditCardPayment({ id }: { id: string }) {
         fullWidth
         open={isModalPaymentOpen}
         onClose={handleClose}
-        PaperProps={{
-          component: "form",
-          onSubmit: handleFormSubmit,
-        }}
         onKeyDown={e => e.key === 'Enter' ? e.preventDefault() : null}
       >
         <DialogTitle>
@@ -81,8 +75,8 @@ export default function CreditCardPayment({ id }: { id: string }) {
             Cancel
           </button>
           <button
+            onClick={handleFormSubmit}
             className="px-2 py-1 text-sm border text-green-500 border-green-300 rounded uppercase hover:bg-green-50 transition ease-in-out"
-            type="submit"
           >
             Add
           </button>
