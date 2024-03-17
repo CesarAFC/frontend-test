bb86b89f3785f14a078462ac919483f3edab8ca5
César Fontalvo <cafc2030@gmail.com>
2024-03-16 23:49:49 -0500

    change:: adds navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import "./App.css";
 import { Provider } from "react-redux";
-// import store from "./store";
 import Products from "./UI/Products";
-import { PersistGate } from 'redux-persist/integration/react'
-import configureStore from './store';
+import { PersistGate } from "redux-persist/integration/react";
+import configureStore from "./store";
+import Navbar from "./components/Navbar";
 
-const {store, persistor} = configureStore()
+const { store, persistor } = configureStore();
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <Products />
+        <section className="bg-white text-zinc-900">
+          <Navbar />
+          <Products />
+        </section>
       </PersistGate>
     </Provider>
   );
